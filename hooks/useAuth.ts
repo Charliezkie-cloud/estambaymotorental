9e4626e08c87d7a8b98bdebf002818327de1b224
Charles Henry Tinoy <charleshenrytinoy8@gmail.com>
2026-07-22 16:29:40 +0800

    Added useAuth.ts hook

---
+++ hooks/useAuth.ts
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { User } from "@supabase/supabase-js";
+
+import { supabaseClient } from "@/lib/supabase-client";
+
+export default function useAuth() {
+  // States
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState<User | null>(null);
+  const [error, setError] = useState("");
+
+  // Use effects
+  useEffect(() => {
+    async function checkAuth() {
+      setLoading(true);
+
+      const { data, error } = await supabaseClient.auth.getUser();
+
+      if (error) return setError(error.message);
+      if (!data) return setUser(null);
+
+      setUser(data.user);
+      return setLoading(false);
+    }
+
+    if (loading) checkAuth();
+  }, [loading]);
+
+  useEffect(() => {
+    function listenAuthChange() {
+      supabaseClient.auth.onAuthStateChange((event, session) => {
+        setLoading(true);
+
+        if (event === "SIGNED_IN" && session) {
+          setUser(session.user);
+        } else if (event === "SIGNED_OUT") {
+          setUser(null);
+        } else {
+          setUser(null);
+        }
+
+        setLoading(false);
+      });
+    }
+
+    if (!loading) listenAuthChange();
+  }, [loading]);
+
+  return { loading, user, error };
+}
