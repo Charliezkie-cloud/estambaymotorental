ba42876a3315d2e1ed54ecbda1b77268d9f2b0ee
Charles Henry Tinoy <charleshenrytinoy8@gmail.com>
2026-07-30 09:09:26 +0800

    Small cleanup.

--- src/components/layouts/vehicles-table/admin-edit-vehicle-dialog.tsx
+++ src/components/layouts/vehicles-table/admin-edit-vehicle-dialog.tsx
@@ -108,18 +108,6 @@ export default function AdminEditVehicleDialog({ supabaseClient, vehicleColors, 
     return false;
   }
 
-  function generateFileName(file: ActualFileObject) {
-    const fileExtension = file.name.split(".").pop()?.toLowerCase();
-    const baseName = file.name
-      .substring(0, file.name.lastIndexOf('.'))
-      .toLowerCase()
-      .replace(/[^a-z0-9]/g, '-')
-      .replace(/-+/g, '-');
-    const uniqueId = crypto.randomUUID();
-
-    return `${uniqueId}-${baseName}.${fileExtension}`;
-  }
-
   // Use effects
   useEffect(() => {
     function mapVehicleColorsRow() {
