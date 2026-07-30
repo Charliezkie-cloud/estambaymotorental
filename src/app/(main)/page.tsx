63e5551694d03e1cc74688dc5b365d946a91d239
Charles Henry Tinoy <charleshenrytinoy8@gmail.com>
2026-07-30 12:55:28 +0800

    Added content to the main page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,8 +3,9 @@ export default function HomePage() {
     <main className="py-12">
 
       <section className="max-w-7xl mx-4 sm:mx-6 md:mx-8 lg:mx-10 xl:mx-auto py-12">
-        <div>
-          <h1 className="text-2xl md:text-4xl font-semibold">Hello world ;D</h1>
+        <div className="space-y-4">
+          <h1 className="text-2xl md:text-4xl font-semibold">This website is currently under development.</h1>
+          <p>We&#39;re currently putting the finishing touches on our new website. We can&#39;t wait to share it with you, so stay tuned!</p>
         </div>
       </section>
 
